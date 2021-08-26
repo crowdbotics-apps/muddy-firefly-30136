be74c55d3762092c9f68085796473633731e55cb
Crowdbotics <team@crowdbotics.com>
2021-08-26 20:27:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "muddy_firefly_30136Identifier",
+  url: "https://muddy_firefly_30136Identifier.botics.co",
+  api: "https://muddy_firefly_30136Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
